d647f703129c0bbb8d7729ed99590aa7446a6019
Ziyang Li <paseaf@me.com>
2023-03-24 16:02:30 +0100

    Install Docker

--- packer/scripts/install_common_software.sh
+++ packer/scripts/install_common_software.sh
@@ -5,3 +5,22 @@ source /var/tmp/scripts/apt_get_wait_lock.sh
 export DEBIAN_FRONTEND=noninteractive
 apt-get update
 apt-get install -y git tmux neovim vim net-tools build-essential
+
+# Docker
+apt-get install -y \
+    ca-certificates \
+    curl \
+    gnupg \
+    lsb-release
+
+# add Docker's official GPG key
+curl -fsSL https://download.docker.com/linux/ubuntu/gpg | gpg --dearmor -o /usr/share/keyrings/docker-archive-keyring.gpg
+
+# set up a stable repo
+echo \
+    "deb [arch=$(dpkg --print-architecture) signed-by=/usr/share/keyrings/docker-archive-keyring.gpg] https://download.docker.com/linux/ubuntu \
+    $(lsb_release -cs) stable" | tee /etc/apt/sources.list.d/docker.list > /dev/null
+
+# install docker engine
+apt-get update
+apt-get install -y docker-ce docker-ce-cli containerd.io docker-compose-plugin
